5824b898116a7efdbd5c55fde03c95b9fa2a84e7
Ayush Raj <ayush.raj2023a@vitstudent.ac.in>
2025-09-20 19:48:57 +0530

    done with the project

--- Horizontal_scroll/app.js
+++ Horizontal_scroll/app.js
@@ -2,7 +2,7 @@ var pg1 = gsap.timeline();
 pg1.from(".logo",{
     opacity : 0,
     y: -20,
-    duration: 1,
+    duration: 0.5,
 })
 
 pg1.from(".others #first li ",{
@@ -16,7 +16,6 @@ pg1.from(".main-image",{
     opacity : 0,
     y: -20,
     duration: 1,
-    stggear: 0.3,
 })
 
 pg1.to("#main-heading", {
@@ -79,14 +78,80 @@ const totalScroll = text.scrollWidth - window.innerWidth;
 
 gsap.to(".Horizontal h1", {
   x: -totalScroll,  
-
+  color : "#9929EA",
   scrollTrigger: {
     trigger: ".Horizontal",
     scroller: "body",
-    markers: true,
+    // markers: true,
     start: "top top",
     end: `+=${totalScroll}`, 
     scrub: true,
     pin: true
   }
 });
+
+
+var footerTimeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".footer",
+    scroller: "body",
+    start: "top 80%",
+    end: "bottom 97.5%",
+  }
+});
+
+
+footerTimeline.from(".footer-about",{
+  y : 100,
+  duration : 0.5,
+  opacity: 0,
+  scale : 0.5
+})
+
+
+
+
+
+footerTimeline.from(".footer-links ul li",{
+  y : 100,
+  duration : 0.5,
+  opacity: 0,
+  scale : 0.5,
+  stagger: -0.1,
+})
+
+footerTimeline.from(".footer-links h3",{
+  y : 100,
+  duration : 0.5,
+  opacity: 0,
+  scale : 0.5
+})
+
+footerTimeline.from(".footer-contact p",{
+  y : 100,
+  duration : 0.5,
+  opacity: 0,
+  scale : 0.5,
+  stagger: -0.1,
+})
+
+
+footerTimeline.from(".footer-contact h3",{
+  y : 100,
+  duration : 0.5,
+  opacity: 0,
+  scale : 0.5
+})
+
+
+footerTimeline.from(".footer-bottom p",{
+  x : -100,
+  duration : 0.5,
+  opacity: 0,
+  // scale : 0.5
+})
+
+
+
+
+
